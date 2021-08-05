476358b4f9c9ee04964c7751b6d032cb67f1c0b0
Oyinda David <oyindadavid18@gmail.com>
2021-08-06 00:14:02 +0100

    ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-     Hello World
+      <span className="heading">Taskify</span>
     </div>
   );
 }
